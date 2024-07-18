14e6cf001c454c26339d2630191770dd0c1bcc33
sarar <sara.ramos@usal.es>
2024-07-18 11:37:32 +0200

    Update AlbumServiceImplTest.java

--- src/test/java/com/example/albumphotoenrichment/service/AlbumServiceImplTest.java
+++ src/test/java/com/example/albumphotoenrichment/service/AlbumServiceImplTest.java
@@ -13,7 +13,6 @@ import java.util.concurrent.ExecutionException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.web.client.RestTemplate;
